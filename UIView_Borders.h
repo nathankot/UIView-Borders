71ac4cfb905c2b72d81cfbed6da2983ed2a13559
Nathan Kot <me@nathankot.com>
2015-09-23 13:23:05 +0900

    Rename umbrella header

--- UIView_Borders.h
+++ UIView_Borders.h
@@ -6,9 +6,9 @@
 //  Copyright © 2015 aaron. All rights reserved.
 //
 
-#ifndef UIViewBorders_h
-#define UIViewBorders_h
+#ifndef UIView_Borders_h
+#define UIView_Borders_h
 
 #import "UIView+Borders.h"
 
-#endif /* UIViewBorders_h */
+#endif /* UIView_Borders_h */
