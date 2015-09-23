a2fc505e5da3092e48242807740c1621bc079b87
Nathan Kot <me@nathankot.com>
2015-09-23 12:50:15 +0900

    Add xcodeproj file

---
+++ UIViewBorders.h
@@ -0,0 +1,14 @@
+//
+//  UIViewBorders.h
+//  UIView-Borders
+//
+//  Created by Nathan Kot on 23/09/15.
+//  Copyright © 2015 aaron. All rights reserved.
+//
+
+#ifndef UIViewBorders_h
+#define UIViewBorders_h
+
+#import "UIView+Borders.h"
+
+#endif /* UIViewBorders_h */
